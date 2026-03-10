fee58ad59dd71a7e3c76c50b0b9c38536a5c15c3
abdulelah-solution </>
2026-03-10 04:09:07 +0300

    Update values

--- utils.py
+++ utils.py
@@ -5,4 +5,4 @@ def get_wsl_host_ip():
     try:
         return subprocess.check_output("ip route show | grep default | awk '{print $3}'", shell=True).decode().strip()
     except:
-        return '172.19.64.1'
+        return 'your_server_ip_or_hostname'
